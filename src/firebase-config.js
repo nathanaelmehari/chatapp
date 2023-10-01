d0b339b32fa421dfb467c96cee407ad92b15b07b
c-19 <92860234+nathanaelmehari@users.noreply.github.com>
2023-10-01 11:11:32 +0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore' 
 const firebaseConfig = {
-  apiKey: "AIzaSyCU-aSypAemb0Tg3JR5eGGI-JSBw-u5vEM",
-  authDomain: "secret-b9c00.firebaseapp.com",
-  projectId: "secret-b9c00",
-  storageBucket: "secret-b9c00.appspot.com",
-  messagingSenderId: "3260901662",
-  appId: "1:3260901662:web:1e6366f884b52db7d8010f"
+  //your api key, auth-token.....etc
 };
 
 // Initialize Firebase
